6fee616b8e07204772769720643307eaf7df69b0
rogierpijpers <rogierpijpers1992@gmail.com>
2016-01-12 20:54:54 +0100

    category nog vergeten...

--- src/domain/businessrule/Category.java
+++ src/domain/businessrule/Category.java
@@ -3,9 +3,22 @@ package domain.businessrule;
 import java.util.ArrayList;
 
 public class Category {
-
 	private String name;
+
+	public Category(String name) {
+		this.name = name;
+	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// ---- for retrieving from database
+
 	public static ArrayList<String> getAll(){
 		return null;
 	}
