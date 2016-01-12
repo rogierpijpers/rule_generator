5892320eaeb6aa6cc9f85ce253b6e905bb91815e
rogierpijpers <rogierpijpers1992@gmail.com>
2016-01-12 14:26:53 +0100

    begin fill template methode

--- src/domain/businessrule/rule/AttributeRange.java
+++ src/domain/businessrule/rule/AttributeRange.java
@@ -3,6 +3,8 @@ package domain.businessrule.rule;
 import org.stringtemplate.v4.ST;
 
 import domain.businessrule.BusinessRule;
+import domain.businessrule.database.Attribute;
+import domain.businessrule.database.Column;
 
 public class AttributeRange extends BusinessRule{
 
@@ -11,16 +13,77 @@ public class AttributeRange extends BusinessRule{
 	private double minValue;
 	private double maxValue;
 	private Operator operator;
-	private Value attribute;
+	private Attribute attribute;
 
 	
 	
 	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(double minValue) {
+		this.minValue = minValue;
+	}
+
+
+	public double getMaxValue() {
+		return maxValue;
+	}
+
+
+	public void setMaxValue(double maxValue) {
+		this.maxValue = maxValue;
+	}
+
+
+	public Operator getOperator() {
+		return operator;
+	}
+
+
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+
+
+	public Attribute getAttribute() {
+		return attribute;
+	}
+
+
+	public void setAttribute(Value attribute) {
+		this.attribute = attribute;
+	}
+	
+
+
 	// ---- fill templates for generation
 	@Override
 	public ST fillTemplate(ST ruleTemplate) {
-		// TODO Auto-generated method stub
-		return null;
+		Column column = (Column) attribute;
+		ruleTemplate.add("targetTable", "");
+		ruleTemplate.add("column", "");
+		ruleTemplate.add("operator", operator.getCharacter());
+		ruleTemplate.add("minValue", minValue);
+		ruleTemplate.add("maxValue", maxValue);
+		return ruleTemplate;
 	}
 
 }
--- src/domain/businessrule/rule/Operator.java
+++ src/domain/businessrule/rule/Operator.java
@@ -4,4 +4,18 @@ public class Operator {
 
 	private String name;
 	private String character;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCharacter() {
+		return character;
+	}
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+	
 }
