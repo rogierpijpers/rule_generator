ebd966c52d6b60deaf6caba2c24cb0924517480b
rogierpijpers <rogierpijpers1992@gmail.com>
2016-01-18 20:41:28 +0100

    businessrules so far

--- src/domain/generator/OracleScriptBuilder.java
+++ src/domain/generator/OracleScriptBuilder.java
@@ -20,80 +20,121 @@ public class OracleScriptBuilder implements ScriptBuilder {
 
 	@Override
 	public String createScript(BusinessRule businessRule) {
-		ST template = templateBuilder.getTemplate(businessRule.getType().getCode(),
-				businessRule.getType().getCodeType());
+		ST template = templateBuilder.getTemplate(businessRule.getType().getCode(), businessRule.getType().getCodeType());
 
 		template = fill(template, businessRule);
 
 		return template.render();
 	}
 	
-	private ST fill(ST template, BusinessRule businessRule){
-		
-		template.add("code", businessRule.getCode());
-		
+	private ST fill(ST template, BusinessRule businessRule){		
+		template.add("code", businessRule.getCode());		
 		switch (businessRule.getType().getCode()) {
 		case "ARNG":
-			AttributeRange ARNGrule = (AttributeRange) businessRule;
-			Column ARNGcolumn = (Column) ARNGrule.getAttribute();
-			template.add("targetTable", ARNGcolumn.getTable().getName());
-			template.add("column", ARNGcolumn.getName());
-			template.add("operator", ARNGrule.getOperator().getCharacter());
-			template.add("minValue", ARNGrule.getMinValue());
-			template.add("maxValue", ARNGrule.getMaxValue());		
+			template =  fillARNG(template, businessRule);		
 			break;
 		case "ACMP":
-			AttributeCompare ACMPrule = (AttributeCompare) businessRule;
-			Column ACMPcolumn = (Column) ACMPrule.getAttribute();
-			template.add("targetTable", ACMPcolumn.getTable().getName());
-			template.add("column", ACMPcolumn.getName());
-			template.add("operator", ACMPrule.getOperator().getCharacter());
-			template.add("value", ACMPrule.getValue());	
+			template = 	fillACMP(template, businessRule);
 			break;
 		case "ALIS":
-			AttributeList ALISrule = (AttributeList) businessRule;
-			Column ALIScolumn = (Column) ALISrule.getAttribute();
-			template.add("targetTable", ALIScolumn.getTable().getName());
-			template.add("column", ALIScolumn.getName());
-			template.add("operator", ALISrule.getOperator().getCharacter());
-			template.add("value", ALISrule.getValueStr());
+			template = 	fillALIS(template, businessRule);
 			break;
 		case "AOTH":
-			AttributeOther AOTHrule = (AttributeOther) businessRule;
-			Column AOTHcolumn = (Column) AOTHrule.getAttribute();
-			template.add("targetTable", AOTHcolumn.getTable().getName());
-			template.add("plsql", AOTHrule.getPlsql());
+			template = 	fillAOTH(template, businessRule);
 			break;
 		case "TCMP":
-			TupleCompare TCMPrule = (TupleCompare) businessRule;
-			Column TCMPcolumn1 = (Column) TCMPrule.getAttribute1();
-			Column TCMPcolumn2 = (Column) TCMPrule.getAttribute2();
-			template.add("targetTable", TCMPcolumn1.getTable());
-			template.add("column1", TCMPcolumn1.getName());
-			template.add("operator", TCMPrule.getOperator().getCharacter());
-			template.add("column2", TCMPcolumn2.getName());
+			template = 	fillTCMP(template, businessRule);
 			break;
 		case "TOTH":
-			TupleOther TOTHrule = (TupleOther) businessRule;
-			Column TOTHcolumn1 = (Column) TOTHrule.getAttribute1();
-			Column TOTHcolumn2 = (Column) TOTHrule.getAttribute2();
-			template.add("targetTable", TOTHcolumn1.getTable());
-			template.add("targetColumn1", TOTHcolumn1.getName());
-			template.add("targetColumn2", TOTHcolumn2.getName());
-			template.add("plsql", TOTHrule.getPlsql());
-			template.add("failureMessage", TOTHrule.getFailureMessage());
+			template = 	fillTOTH(template, businessRule);
 			break;
 		case "ICMP":
-			
+			template = 	fillICMP(template, businessRule);
 			break;
 		case "EOTH":
-			
+			template =  fillEOTH(template, businessRule);	
 			break;
 		case "MODI":
-			
+			template = 	fillMODI(template, businessRule);
 			break;
-		}
-		
+		}	
 		return template;
+	}
+	
+	private ST fillARNG(ST template, BusinessRule businessRule){
+		AttributeRange ARNGrule = (AttributeRange) businessRule;
+		Column ARNGcolumn = (Column) ARNGrule.getAttribute();
+		template.add("targetTable", ARNGcolumn.getTable().getName());
+		template.add("column", ARNGcolumn.getName());
+		template.add("operator", ARNGrule.getOperator().getCharacter());
+		template.add("minValue", ARNGrule.getMinValue());
+		template.add("maxValue", ARNGrule.getMaxValue());
+		return template;
+	}
+	
+	private ST fillACMP(ST template, BusinessRule businessRule){
+		AttributeCompare ACMPrule = (AttributeCompare) businessRule;
+		Column ACMPcolumn = (Column) ACMPrule.getAttribute();
+		template.add("targetTable", ACMPcolumn.getTable().getName());
+		template.add("column", ACMPcolumn.getName());
+		template.add("operator", ACMPrule.getOperator().getCharacter());
+		template.add("value", ACMPrule.getValue());	
+		return template;
+	}
+	
+	private ST fillALIS(ST template, BusinessRule businessRule){
+		AttributeList ALISrule = (AttributeList) businessRule;
+		Column ALIScolumn = (Column) ALISrule.getAttribute();
+		template.add("targetTable", ALIScolumn.getTable().getName());
+		template.add("column", ALIScolumn.getName());
+		template.add("operator", ALISrule.getOperator().getCharacter());
+		template.add("value", ALISrule.getValueStr());
+		return template;
+	}
+	
+	private ST fillAOTH(ST template, BusinessRule businessRule){
+		AttributeOther AOTHrule = (AttributeOther) businessRule;
+		Column AOTHcolumn = (Column) AOTHrule.getAttribute();
+		template.add("targetTable", AOTHcolumn.getTable().getName());
+		template.add("plsql", AOTHrule.getPlsql());
+		return template;
+	}
+	
+	private ST fillTCMP(ST template, BusinessRule businessRule){
+		TupleCompare TCMPrule = (TupleCompare) businessRule;
+		Column TCMPcolumn1 = (Column) TCMPrule.getAttribute1();
+		Column TCMPcolumn2 = (Column) TCMPrule.getAttribute2();
+		template.add("targetTable", TCMPcolumn1.getTable());
+		template.add("column1", TCMPcolumn1.getName());
+		template.add("operator", TCMPrule.getOperator().getCharacter());
+		template.add("column2", TCMPcolumn2.getName());
+		return template;
+	}
+	
+	private ST fillTOTH(ST template, BusinessRule businessRule){
+		TupleOther TOTHrule = (TupleOther) businessRule;
+		Column TOTHcolumn1 = (Column) TOTHrule.getAttribute1();
+		Column TOTHcolumn2 = (Column) TOTHrule.getAttribute2();
+		template.add("targetTable", TOTHcolumn1.getTable());
+		template.add("targetColumn1", TOTHcolumn1.getName());
+		template.add("targetColumn2", TOTHcolumn2.getName());
+		template.add("plsql", TOTHrule.getPlsql());
+		template.add("failureMessage", TOTHrule.getFailureMessage());
+		return template;
+	}
+	
+	private ST fillICMP(ST template, BusinessRule businessRule){
+		
+		return null;
+	}
+	
+	private ST fillEOTH(ST template, BusinessRule businessRule){
+		
+		return null;
+	}
+	
+	private ST fillMODI(ST template, BusinessRule businessRule){
+		
+		return null;
 	}
 }
