7938e6736cd00a03400b0217b94f09d0d9cc032c
rogierpijpers <rogierpijpers1992@gmail.com>
2016-01-19 17:26:06 +0100

    table attributen aangepast

--- src/domain/businessrule/database/Table.java
+++ src/domain/businessrule/database/Table.java
@@ -4,6 +4,8 @@ package domain.businessrule.database;
 public class Table {
 	private String name;
 	private TargetDatabase targetDatabase;
+	private String primaryKey;
+	private String foreignKey;
 	
 	public Table(String name, TargetDatabase targetDatabase){
 		this.setName(name);
@@ -25,6 +27,23 @@ public class Table {
 	public void setTargetDatabase(TargetDatabase targetDatabase) {
 		this.targetDatabase = targetDatabase;
 	}
+
+	public String getPrimaryKey() {
+		return primaryKey;
+	}
+
+	public void setPrimaryKey(String primaryKey) {
+		this.primaryKey = primaryKey;
+	}
+
+	public String getForeignKey() {
+		return foreignKey;
+	}
+
+	public void setForeignKey(String foreignKey) {
+		this.foreignKey = foreignKey;
+	}
+	
 	
 	
 }
