ddaeacdf19c673fa351b239563b2b88527870198
Nikky Bruijn <nikkybruijn@gmail.com>
2017-03-12 20:25:44 +0100

    Resolved Issues
    FileSaver now uses the try-with-resource statement.
    GeneratorController no longer has the local variable "service"

--- src/data/utility/FileSaver.java
+++ src/data/utility/FileSaver.java
@@ -8,19 +8,10 @@ public class FileSaver {
 
 	public static void writeFile(String script, String name, String directory,
 			String extention) {
-		BufferedWriter writer = null;
-		try {
-			writer = new BufferedWriter(new FileWriter(directory + "\\" + name
-					+ "." + extention));
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(directory + "\\" + name + "." + extention))){
 			writer.write(script);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
